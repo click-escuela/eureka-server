df487b4c51159c8fa7c4c68af21a47b8e63d3cfd
Evelin Aragon <eve_aragon@hotmail.com>
2021-08-22 00:29:40 -0300

    actualizacion de scripts2

---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+chmod +x /home/ec2-user/server/eureka
+chmod +x /home/ec2-user/server/eureka/*.jar
+chmod +x /home/ec2-user/server/eureka/server_start.sh
+chmod +x /home/ec2-user/server/eureka/server_stop.sh
+chmod +x /home/ec2-user/server/eureka/server_clear.sh
+chmod +x /home/ec2-user/server/eureka/fix_privileges.sh
