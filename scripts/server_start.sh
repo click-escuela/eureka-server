a01f627c9bd9ad614e98bcab0818de5000973d6c
clickescuela <83097465+clickescuela@users.noreply.github.com>
2021-08-24 21:48:05 -0300

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,5 +5,4 @@ sudo rm -rf /home/ec2-user/server/eureka/eureka-service.pid
 
 echo"eliminando archivo"
 
-sudo java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-east-2.rds.amazonaws.com:3306/clickescuela -Dspring.datasource.username=root -Dspring.datasource.password=secret123 \
-    eureka-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > eureka-service.pid
+sudo java -jar eureka-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > eureka-service.pid
