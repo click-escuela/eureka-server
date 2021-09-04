486a85f7ba5f7eb06cc79ea3fb2068e87dbda28a
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 20:21:57 -0300

    fix logs

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod +x /home/ec2-user/server/eureka/logs
+chmod +x /home/ec2-user/server/eureka/logs/error.log
+chmod +x /home/ec2-user/server/eureka/logs/debug.log
 var="$(cat /home/ec2-user/server/eureka/eureka-service.pid)"
 sudo kill $var
 sudo rm -rf /home/ec2-user/server/eureka/eureka-service.pid
