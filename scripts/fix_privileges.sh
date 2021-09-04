eea4bc706d9caad78886c42bdef98c9c1d319422
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 20:25:51 -0300

    fix logs

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -5,6 +5,4 @@ chmod +x /home/ec2-user/server/eureka/server_start.sh
 chmod +x /home/ec2-user/server/eureka/server_stop.sh
 chmod +x /home/ec2-user/server/eureka/server_clear.sh
 chmod +x /home/ec2-user/server/eureka/fix_privileges.sh
-chmod +x /home/ec2-user/server/eureka/logs
-chmod +x /home/ec2-user/server/eureka/logs/error.log
-chmod +x /home/ec2-user/server/eureka/logs/debug.log
+
