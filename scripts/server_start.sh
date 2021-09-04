35fa93c7539c386e07b479d74dcb1cc34d6cdbc0
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 14:01:05 -0300

    logs

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,4 +5,4 @@ sudo rm -rf /home/ec2-user/server/eureka/eureka-service.pid
 
 echo"eliminando archivo"
 
-sudo java -jar eureka-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > eureka-service.pid
+sudo java -jar -Dlogging.file.name=/home/ec2-user/server/eureka/debug.log eureka-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > eureka-service.pid
