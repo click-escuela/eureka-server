a08fb0c5451799a7f3dfb05e60a77aa3e6db69b7
Evelin <eve_aragon@hotmail.com>
2021-10-19 22:30:00 -0300

    subir a dev

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/eureka
+sudo rm -rf /home/ec2-user/server/eureka/appspec.yml
+sudo rm -rf /home/ec2-user/server/eureka/eureka-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/eureka/eureka-service.pid
+sudo rm -rf /home/ec2-user/server/eureka/logs
+sudo rm -rf /home/ec2-user/server/eureka/server_stop.sh
+sudo rm -rf /home/ec2-user/server/eureka/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/eureka/server_prestart.sh
+sudo rm -rf /home/ec2-user/server/eureka/server_clear.sh
--- scripts/server_start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ec2-user/server/eureka
-
-sudo rm -rf /home/ec2-user/server/eureka/eureka-service.pid
-
-echo"eliminando archivo"
-
-sudo java -jar -Dlogging.file.name=/home/ec2-user/server/eureka/debug.log eureka-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > eureka-service.pid
